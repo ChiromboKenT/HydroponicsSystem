a9b96f9cd82fdea329d1d24c6d6cea4ffe150189
Kenny Chirombo <57112312+ChiromboKenT@users.noreply.github.com>
2021-10-18 22:34:31 +0200

    C-B

--- src/index.js
+++ src/index.js
@@ -1,26 +1,56 @@
 const dataPin = 5;
+const mcpadc = require("mcp-spi-adc")
 const dhtSensor =  require('node-dht-sensor')
 
+//DefineIntervals
+let WLInterval;
+let DHTInterval;
+let TDSInterval;
 
-setInterval(() => { 
-    	dhtSensor.read(11,4, (err,temperature,humidity) => {
-		try{
-			console.log(`temp: ${temperature}°C, humidity: ${humidity}%`);
-		}catch(err){
+const WaterLevelMeter = mcpadc.open(7, {speedHz: 20000}, err => {
+	if (err) throw err;
+	WLInterval = setInterval(_ => {
+		WaterLevelMeter.read((err, reading) => {
+		if (err) throw err;
+  
+		console.log(`Water Level: ${(reading.value * 3.3 - 0.5) * 100}`);
+	  });
+	}, 1000);
+  });
 
-			console.log(err);
-		}
-	})
+const TDSReading = mcpadc.open(6, {speedHz: 20000}, err => {
+	if (err) throw err;
+	TDSInterval = setInterval(_ => {
+		TDSReading.read((err, reading) => {
+		if (err) throw err;
+
+		console.log(`TDS: ${(reading.value * 3.3 - 0.5) * 100}`);
+		});
+	}, 1000);
+});
+
+DHTInterval = setInterval(() => { 
+	dhtSensor.read(11,4, (err,temperature,humidity) => {
+	try{
+		console.log(`temp: ${temperature}°C, humidity: ${humidity}%`);
+	}catch(err){
+		console.log(err);
+	}
+})
 }, 2500); // the sensor can only be red every 2 seconds
  
 
  const close =  () => {
     try{
        
-       console.log("Exit");
+		clearInterval(DHTInterval);
+		clearInterval(TDSInterval);
+		clearInterval(WLInterval);
+       	console.log("Exit");
+
       }
       catch(err){
-          console.log(err);
+        console.log(err);
       }
  }
 process.on("SIGINT", close);
