56a468e06af5234878d6d0fa5f520c7385e31e9e
ChiromboKenT <Chirombokenny@gmail.com>
2021-10-18 20:06:42 +0100

    A-S

--- src/index.js
+++ src/index.js
@@ -1,20 +1,18 @@
 const dataPin = 5;
-const dht =  require('pigpio-dht')
-const sensor = dht(dataPin, 11);
+const dhtSensor =  require('node-dht-sensor')
 
 
 setInterval(() => { 
-    sensor.read();
+    	dhtSensor.read(11,4, (err,temperature,humidity) => {
+		try{
+			console.log(`temp: ${temperature}°C, humidity: ${humidity}%`);
+		}catch(err){
+
+			console.log(err);
+		}
+	})
 }, 2500); // the sensor can only be red every 2 seconds
  
-sensor.on('result', data => {
-    console.log(`temp: ${data.temperature}°c`); 
-    console.log(`rhum: ${data.humidity}%`); 
-});
- 
-sensor.on('badChecksum', () => {
-    console.log('checksum failed');
-});
 
  const close =  () => {
     try{
