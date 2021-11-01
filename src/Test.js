7a91f4293e2cc13ab5c003619b5bca6797b37a98
Kenny Chirombo <57112312+ChiromboKenT@users.noreply.github.com>
2021-11-01 16:41:22 +0200

    BB-C

--- src/Test.js
+++ src/Test.js
@@ -1,16 +1,17 @@
 pHValues  = [110, 109,110,110,110,109,110,208,208,208,206,208,407,407,408,409,408,408,408,598,598,598,790,791,791,791,999,999,998,999] 
 waterLevel = [0, 1, 5, 7,10,11,14,18,19,20,22,26,25,25,25,25,25,25,25,25,25,25,25,25,24,24,24,24,25]
+luxValue = [0.9,1,0.8,0.7,0.8,0.6,0.6,0.6,0.7]
 
 // pHValues.forEach(element => {
 //     console.log(`TDS Sensor Reading: ${element}`)
 // });
 
-waterLevel.forEach(element => {
+luxValue.forEach(element => {
     
-    console.log(`Current Water Level: ${element}mm`)
-    if(element < 25){
-        console.log(`Water Pump: ON`)
+    console.log(`Current LUX: ${element}mm`)
+    if(element < 1){
+        console.log(`Grow Lights: ON`)
     }else{
-        console.log("Water Pump: OFF")
+        console.log("Grow Lights: OFF")
     }
 })
