b091cc77b2117e06ee92194f3c7f45f17a78f226
Kenny Chirombo <57112312+ChiromboKenT@users.noreply.github.com>
2021-11-10 23:22:12 +0200

    AC

--- src/Test.js
+++ src/Test.js
@@ -8,6 +8,13 @@ const pwm = require('raspi-soft-pwm');
 const events = require("events");
 const {TempSensor} = require("./TempSensor.js")
 
+
+//DefineIntervals
+let WLInterval;
+let DHTInterval;
+let TDSInterval;
+let PhInterval;
+
 //Define Pump State
  const flowState = {
 	 inlet : 1,
@@ -15,28 +22,58 @@ const {TempSensor} = require("./TempSensor.js")
 	 valve : 0
  }	
 
-
 //Define GPIO Pins
+let Cycle = 100;
+let fan;
+const tempPin = 4;
+const WaterLevelChannel = 7
+const TDSChannel = 6;
+const phChannel = 5
+const inletPump  = new Gpio(26, 'out'); //Channel 1
+const outletPump = new Gpio(20, 'out');	//Channel 2
+const valve = new Gpio(21, 'out');		//Channel 3
 
-const inletPump  = new Gpio(26, 'out');
-const outletPump = new Gpio(20, 'out');
-const valve = new Gpio(21, 'out');
 
 inletPump.writeSync(0);
-outletPump.writeSync(0)
-let count = 0;
+outletPump.writeSync(0);
 
-const countInterval = setInterval(() => {
-    count ++;
-    if(count % 3 == 0){
-        console.log(count)
-        inletPump.writeSync(1);
-    }
-}, 1500)
- const close =  () => {
+
+
+ const WaterLevelMeter = mcpadc.open(WaterLevelChannel, {speedHz: 20000}, err => {
+	if (err) throw err;
+	WLInterval = setInterval(_ => {
+		WaterLevelMeter.read((err, reading) => {
+		if (err) throw err;
+		const waterLevel = (reading.value * 3.3 - 0.5) * 100
+		if( waterLevel <= 200){
+            console.log("Inlet Pump On");
+			swithInletPump(1) //Water In
+		}else{
+            console.log("Inlet Pump OFF");
+			swithInletPump(0);
+		}
+		console.log(`Water Level: ${waterLevel}`);
+	  });
+	}, 1000);
+  });
+
+const swithInletPump = (state) => {
+	inletPump.writeSync(state);
+	flowState.inlet = state
+}
+const swithOutletPump = (state) => {
+	outletPump.writeSync(state);
+	flowState.outlet = state
+}
+const swithOutletValve = (state) => {
+	valve.writeSync(state);	
+	flowState.valve = state
+}
+
+const close =  () => {
     try{
 		
-		clearInterval(countInterval);
+        clearInterval(WLInterval)
        	console.log("Exit");
 
       }
@@ -44,6 +81,4 @@ const countInterval = setInterval(() => {
         console.log(err);
       }
  }
-
-
 process.on("SIGINT", close);
