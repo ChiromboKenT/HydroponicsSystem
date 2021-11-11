e81e86edfbdbf8ffd89fcaf01c6d97429eb87449
ChiromboKenT <Chirombokenny@gmail.com>
2021-11-11 14:57:12 +0000

    AT

--- src/Test.js
+++ src/Test.js
@@ -2,6 +2,7 @@ const dataPin = 5;
 const mcpadc = require("mcp-spi-adc")
 const Gpio = require('onoff').Gpio;
 const dhtSensor =  require('node-dht-sensor').promises
+const mqtt = require("mqtt");
 const process  = require('process');
 
 const raspi = require('raspi');
@@ -9,6 +10,43 @@ const pwm = require('raspi-soft-pwm');
 const EventEmitter = require("events");
 
 
+const options = {
+	clientId : "mqqtRaspi1k",
+}
+ const opitons2 = {
+	qos:0,
+	retain:false
+}
+let client = mqtt.connect("mqtt://test.mosquitto.org",options)
+
+const topicPH = "raspi-ken/ph";
+const topicTemp = "raspi-ken/temp";
+const topicHumidity = "raspi-ken/humidity";
+const topicTDS = "raspi-ken/tds";
+const topicInlet = "raspi-ken/inlet";
+const topicOutlet = "raspi-ken/outlet";
+const topicFan = "raspi-ken/fan"
+
+const topicWater = "raspi-ken/water"
+
+//DefineIntervals
+let WLInterval;
+let DHTInterval;
+let TDSInterval;
+let PhInterval;
+
+
+client.on("connect",() => {
+	console.log("Connected");
+	client.subscribe(["raspi-ken/lights"], () => {
+		console.log("Subscribed to topicPH");
+	})
+	client.publish(topicPH, "6.4" , {qos:1,retain:true}, (error) => {
+	if(error){
+		console.log(error);
+	}
+	})
+	
 //Define GPIO Pins
 let Cycle = 100;
 let fan;
@@ -33,20 +71,24 @@ dht11Emmitter = new EventEmitter()
 dht11Emmitter.on("Read", (Value) => {
 	if((Value.Temperature >= 23)){
 		fan.write(Cycle/100);
+		client.publish(topicFan, "true", {qos:0,retain:true}, (error) => {
+			if(error){
+				console.log(error);
+			}
+		})
 		console.log("Fan Turned ON");
 	} else{
 		fan.write(0);
+		client.publish(topicFan, "false", {qos:0,retain:true}, (error) => {
+			if(error){
+				console.log(error);
+			}
+		})
 		console.log("Fan Turned OFF");
 	}
 })  
 
 
-
-//DefineIntervals
-let WLInterval;
-let DHTInterval;
-let TDSInterval;
-let PhInterval;
 
 //Define Pump State
  const flowState = {
@@ -73,14 +115,30 @@ valve.writeSync(relayOFF);
 		if (err) throw err;
 		const waterLevel = (reading.value * 3.3 - 0.5) * 100
 		if( waterLevel >= 150){
-            console.log("Inlet Pump On");
+				
+	            console.log("Inlet Pump On");
 			swithInletPump(relayON) //Water In
+			client.publish(topicInlet, "true", {qos:0,retain:true}, (error) => {
+			if(error){
+				console.log(error);
+			}
+			})
 		}else{
             
             console.log("Inlet Pump OFF");
 			swithInletPump(relayOFF);
+			client.publish(topicInlet, "false", {qos:0,retain:false}, (error) => {
+			if(error){
+				console.log(error);
+			}
+		})
 		}
 		console.log(`Water Level: ${waterLevel}`);
+		client.publish(topicWater, `${waterLevel}`, {qos:0,retain:false}, (error) => {
+			if(error){
+				console.log(error);
+			}
+		})
 	  });
 	}, 1000);
   });
@@ -99,6 +157,16 @@ DHTInterval = setInterval(async () => {
 			`temp: ${Data.Temperature}°C, ` +
 			`humidity: ${Data.Humidity}%`
 		);
+		client.publish(topicHumidity, `${Data.Humidity}`, {qos:0,retain:false}, (error) => {
+			if(error){
+				console.log(error);
+			}
+		})
+		client.publish(topicTemp, `${Data.Temperature}`, {qos:0,retain:false}, (error) => {
+			if(error){
+				console.log(error);
+			}
+		})
 		dht11Emmitter.emit("Read",Data); 
 	}
 	catch(err){
@@ -114,16 +182,31 @@ const TDSReading = mcpadc.open(TDSChannel, {speedHz: 20000}, err => {
 		if (err) throw err;
 		const tdsVal = (reading.value * 3.3 - 0.5) * 100 + 50;
 		console.log(`TDS Level: ${tdsVal}`);
+		client.publish(topicTDS, `${tdsVal}`, {qos:0,retain:false}, (error) => {
+			if(error){
+				console.log(error);
+			}
+		})
 		if(tdsVal <= 120){
 			console.log("OUTLET VALVE ON")
 			swithOutletValve(relayON);
 			console.log("OUTLET Pump ON");
 			swithOutletPump(relayON);
+			client.publish(topicOutlet, "true", {qos:0,retain:false}, (error) => {
+			if(error){
+				console.log(error);
+			}
+			})
 		}else{
 			console.log("OUTLET VALVE OFF ")
 			swithOutletValve(relayOFF);
 			console.log("OUTLET Pump OFF");
 			swithOutletPump(relayOFF);
+			client.publish(topicOutlet, "false", {qos:0,retain:false}, (error) => {
+			if(error){
+				console.log(error);
+			}
+			})
 		}
 	
 		
@@ -144,6 +227,13 @@ const swithOutletValve = (state) => {
 	flowState.valve = state
 }
 
+}
+	
+})
+
+client.on("message", (topic,payload) => {
+	console.log(`Message Received: ${topic}: ${payload}`);
+})
 const close =  () => {
     try{
 		
@@ -158,10 +248,11 @@ const close =  () => {
       catch(err){
         console.log(err);
       }
- }
+ 
 
  process.on('beforeExit', (code) => {
 	console.log('Process beforeExit event with code: ', code);
+
   });
 process.stdin.resume();
 process.on("SIGINT", close);
