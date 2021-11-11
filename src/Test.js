fe6110c14d28d579857953a07720192f4c8c8b97
Kenny Chirombo <57112312+ChiromboKenT@users.noreply.github.com>
2021-11-11 18:09:17 +0200

    AT

--- src/Test.js
+++ src/Test.js
@@ -9,13 +9,15 @@ const raspi = require('raspi');
 const pwm = require('raspi-soft-pwm');
 const EventEmitter = require("events");
 
-
+const controlState = {
+	auto : 1
+}
 const options = {
 	clientId : "mqqtRaspi1k",
 }
  const opitons2 = {
-	qos:0,
-	retain:false
+	qos:1,
+	retain:true
 }
 let client = mqtt.connect("mqtt://test.mosquitto.org",options)
 
@@ -34,11 +36,56 @@ let WLInterval;
 let DHTInterval;
 let TDSInterval;
 let PhInterval;
+let UCInterval;
 
+const inletPump  = new Gpio(26, 'out'); //Channel 1
+const outletPump = new Gpio(20, 'out');	//Channel 2
+const valve = new Gpio(21, 'out');		//Channel 3
+
+//Define GPIO Pins
+let Cycle = 100;
+let fan;
+const tempPin = 4;
+const WaterLevelChannel = 7
+const TDSChannel = 6;
+const phChannel = 5
+const relayOFF = 1;
+const relayON = 0;
+
+//Define Pump State
+const flowState = {
+	inlet : 1,
+	outlet : 0,
+	valve : 0
+}	
+let ControlState = 0;
+inletPump.writeSync(relayOFF);
+outletPump.writeSync(relayOFF);
+valve.writeSync(relayOFF);
+
+
+raspi.init(() => {
+	fan = new pwm.SoftPWM({pin:'GPIO17',frequency:120});
+	// 50% Duty Cycle, aka half brightness
+	fan.write(0);
+});
+
+const swithInletPump = (state) => {
+	inletPump.writeSync(state);
+	flowState.inlet = state
+}
+const swithOutletPump = (state) => {
+	outletPump.writeSync(state);
+	flowState.outlet = state
+}
+const swithOutletValve = (state) => {
+	valve.writeSync(state);	
+	flowState.valve = state
+}
 
 client.on("connect",() => {
 	console.log("Connected");
-	client.subscribe(["raspi-ken/lights"], () => {
+	client.subscribe(["raspi-ken/lights", "raspi-ken/fan"], () => {
 		console.log("Subscribed to topicPH");
 	})
 	client.publish(topicPH, "6.4" , {qos:1,retain:true}, (error) => {
@@ -47,66 +94,35 @@ client.on("connect",() => {
 		}
 	})
 	
-	//Define GPIO Pins
-	let Cycle = 100;
-	let fan;
-	const tempPin = 4;
-	const WaterLevelChannel = 7
-	const TDSChannel = 6;
-	const phChannel = 5
-	const relayOFF = 1;
-	const relayON = 0;
-
-	//Define TempSensor
-
-	raspi.init(() => {
-		fan = new pwm.SoftPWM({pin:'GPIO17',frequency:120});
-		// 50% Duty Cycle, aka half brightness
-		fan.write(0);
-	});
+	
 
 
 	//Define events
 	dht11Emmitter = new EventEmitter()
 	dht11Emmitter.on("Read", (Value) => {
 		if((Value.Temperature >= 23)){
-			fan.write(Cycle/100);
-			client.publish(topicFan, "true", {qos:0,retain:true}, (error) => {
+			if(controlState == 0){
+				fan.write(Cycle/100);
+			}
+			client.publish(topicFan, "true", {qos:1,retain:true}, (error) => {
 				if(error){
 					console.log(error);
 				}
 			})
 			console.log("Fan Turned ON");
 		} else{
-			fan.write(0);
-			client.publish(topicFan, "false", {qos:0,retain:true}, (error) => {
+			if(controlState == 0){
+				fan.write(0);
+			}
+			
+			client.publish(topicFan, "false", {qos:1,retain:true}, (error) => {
 				if(error){
 					console.log(error);
 				}
 			})
 			console.log("Fan Turned OFF");
 		}
-	})  
-
-
-
-	//Define Pump State
-	const flowState = {
-		inlet : 1,
-		outlet : 0,
-		valve : 0
-	}	
-
-
-	const inletPump  = new Gpio(26, 'out'); //Channel 1
-	const outletPump = new Gpio(20, 'out');	//Channel 2
-	const valve = new Gpio(21, 'out');		//Channel 3
-
-
-	inletPump.writeSync(relayOFF);
-	outletPump.writeSync(relayOFF);
-	valve.writeSync(relayOFF);
-
+	});  
 
 	const WaterLevelMeter = mcpadc.open(WaterLevelChannel, {speedHz: 20000}, err => {
 		if (err) throw err;
@@ -114,27 +130,30 @@ client.on("connect",() => {
 			WaterLevelMeter.read((err, reading) => {
 			if (err) throw err;
 			const waterLevel = (reading.value * 3.3 - 0.5) * 100
-			if( waterLevel >= 150){
+			if(controlState == 0){
+				if( waterLevel >= 150){
 					
 					console.log("Inlet Pump On");
-				swithInletPump(relayON) //Water In
-				client.publish(topicInlet, "true", {qos:0,retain:true}, (error) => {
-				if(error){
-					console.log(error);
-				}
+					swithInletPump(relayON) //Water In
+					client.publish(topicInlet, "true", {qos:1,retain:true}, (error) => {
+						if(error){
+							console.log(error);
+						}
+					})
+				}else{
+					
+					console.log("Inlet Pump OFF");
+					swithInletPump(relayOFF);
+					client.publish(topicInlet, "false", {qos:1,retain:true}, (error) => {
+					if(error){
+						console.log(error);
+					}
 				})
-			}else{
-				
-				console.log("Inlet Pump OFF");
-				swithInletPump(relayOFF);
-				client.publish(topicInlet, "false", {qos:0,retain:false}, (error) => {
-				if(error){
-					console.log(error);
 				}
-			})
 			}
 			console.log(`Water Level: ${waterLevel}`);
-			client.publish(topicWater, `${waterLevel}`, {qos:0,retain:false}, (error) => {
+			const waterValue = waterLevel <= 150 ? 100 : 280 - waterLevel;
+			client.publish(topicWater, `${waterValue > 0 ? waterValue : waterValue * -1}`, {qos:1,retain:true}, (error) => {
 				if(error){
 					console.log(error);
 				}
@@ -157,12 +176,12 @@ client.on("connect",() => {
 				`temp: ${Data.Temperature}°C, ` +
 				`humidity: ${Data.Humidity}%`
 			);
-			client.publish(topicHumidity, `${Data.Humidity}`, {qos:0,retain:false}, (error) => {
+			client.publish(topicHumidity, `${Data.Humidity}`, {qos:1,retain:true}, (error) => {
 				if(error){
 					console.log(error);
 				}
 			})
-			client.publish(topicTemp, `${Data.Temperature}`, {qos:0,retain:false}, (error) => {
+			client.publish(topicTemp, `${Data.Temperature}`, {qos:1,retain:true}, (error) => {
 				if(error){
 					console.log(error);
 				}
@@ -182,7 +201,7 @@ client.on("connect",() => {
 			if (err) throw err;
 			const tdsVal = (reading.value * 3.3 - 0.5) * 100 + 50;
 			console.log(`TDS Level: ${tdsVal}`);
-			client.publish(topicTDS, `${tdsVal}`, {qos:0,retain:false}, (error) => {
+			client.publish(topicTDS, `${tdsVal}`, {qos:1,retain:true}, (error) => {
 				if(error){
 					console.log(error);
 				}
@@ -192,7 +211,7 @@ client.on("connect",() => {
 				swithOutletValve(relayON);
 				console.log("OUTLET Pump ON");
 				swithOutletPump(relayON);
-				client.publish(topicOutlet, "true", {qos:0,retain:false}, (error) => {
+				client.publish(topicOutlet, "true", {qos:1,retain:true}, (error) => {
 				if(error){
 					console.log(error);
 				}
@@ -202,7 +221,7 @@ client.on("connect",() => {
 				swithOutletValve(relayOFF);
 				console.log("OUTLET Pump OFF");
 				swithOutletPump(relayOFF);
-				client.publish(topicOutlet, "false", {qos:0,retain:false}, (error) => {
+				client.publish(topicOutlet, "false", {qos:1,retain:true}, (error) => {
 				if(error){
 					console.log(error);
 				}
@@ -212,26 +231,20 @@ client.on("connect",() => {
 			
 		});
 		}, 2000);
-	});
-
-	const swithInletPump = (state) => {
-		inletPump.writeSync(state);
-		flowState.inlet = state
-	}
-	const swithOutletPump = (state) => {
-		outletPump.writeSync(state);
-		flowState.outlet = state
-	}
-	const swithOutletValve = (state) => {
-		valve.writeSync(state);	
-		flowState.valve = state
-	}
-	
-})
+	});	
+});
 
 client.on("message", (topic,payload) => {
 	console.log(`Message Received: ${topic}: ${payload}`);
-})
+
+	if(topic == "raspi-ken/inlet"){
+		swithInletPump(payload)
+	}
+	controlState = 1;
+	UCInterval = setTimeout(() => {
+		controlState = 0;
+	},10000)
+});
 const close =  () => {
     try{
 		
@@ -240,6 +253,7 @@ const close =  () => {
 		valve.writeSync(relayOFF);
         clearInterval(WLInterval)
 		clearInterval(DHTInterval);
+		clearTimeout(UCInterval);
        	console.log("Exit");
 
       }
